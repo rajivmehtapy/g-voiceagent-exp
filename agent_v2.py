7a322e12802d74feeb70690a9dcd322042df6d06
rajivmehtapy <rajiv.mehta@bacancy.com>
2025-06-21 22:46:33 +0530

    feat: implement voice agent with Deepgram STT and Cartesia TTS

---
+++ agent_v2.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions
+from livekit.plugins import (
+    openai,
+    google,
+    cartesia,
+    deepgram,
+    noise_cancellation,
+    silero,
+)
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+
+load_dotenv()
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(instructions="You are a helpful voice AI assistant.")
+
+
+async def entrypoint(ctx: agents.JobContext):
+
+    # session = AgentSession(
+    #     tts = google.TTS(gender="female",voice_name="en-US-Standard-H",),
+    #     llm=openai.LLM(model="gpt-4.1-nano"),
+    #     stt = google.STT(model="chirp", spoken_punctuation=False),
+    #     vad=silero.VAD.load(),
+    #     turn_detection=MultilingualModel(),
+    # )
+
+    session = AgentSession(
+        stt=deepgram.STT(model="nova-3", language="multi"),
+        llm=openai.LLM(model="gpt-4.1-nano"),
+        tts=cartesia.TTS(model="sonic-2", voice="f786b574-daa5-4673-aa0c-cbe3e8534c02"),
+        vad=silero.VAD.load(),
+        turn_detection=MultilingualModel(),
+    )
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            # LiveKit Cloud enhanced noise cancellation
+            # - If self-hosting, omit this parameter
+            # - For telephony applications, use `BVCTelephony` for best results
+            noise_cancellation=noise_cancellation.BVC(), 
+        ),
+    )
+
+    await ctx.connect()
+
+    await session.generate_reply(
+        instructions="Greet the user and offer your assistance."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
